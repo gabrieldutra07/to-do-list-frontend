34736b178646e6f63b47dfe68cbe392f6b1a821c
Gabriel Dutra <gabrielcbdutra@gmail.com>
2024-05-03 21:35:45 -0300

    Validação de login funcionando, buscando no banco de dados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function validateIsAuth() {
+    const isAuth = localStorage.getItem("isAuth")
+
+    if(!isAuth) {
+        window.location.href = 'index.html'
+    }
+}
+
 function createUser() {
     const email = document.getElementById("username").value
     const password = document.getElementById("password").value
@@ -11,7 +19,7 @@ function createUser() {
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-            if(this.status === 201) {
+            if (this.status === 201) {
                 alert("Cadastro realizado com sucesso!")
                 window.location.href = 'index.html'
             }
@@ -20,6 +28,35 @@ function createUser() {
     });
 
     xhr.open("POST", "http://localhost:8080/api/todolist/user/create");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+}
+
+function login() {
+    const email = document.getElementById("username").value
+    const password = document.getElementById("password").value
+    var data = JSON.stringify({
+        "email": email,
+        "password": password
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            if(this.status === 200) {
+                window.location.href = 'to-do-list.html'
+                localStorage.setItem("isAuth", true)
+            } else if (this.status === 404) {
+                document.getElementById("login-incorrect").removeAttribute("hidden")
+            }
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "http://localhost:8080/api/todolist/user/login");
     xhr.setRequestHeader("Content-Type", "application/json");
 
     xhr.send(data);
